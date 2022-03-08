491da9ff89b3d9922daa72de4098b556065e7ea7
shanksfk <shanksfk@gmail.com>
2022-03-08 23:50:09 +0800

    last

--- afeng/settings.py
+++ afeng/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [    BASE_DIR / "static",]
 
-STATICFILES_DIRS= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
