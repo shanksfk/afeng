511f7b52225a897c1f6d611081b60946be41c655
shanksfk <shanksfk@gmail.com>
2022-07-22 11:42:38 +0800

    add one file

--- afeng/settings.py
+++ afeng/settings.py
@@ -9,9 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django_heroku
-import dj_database_url
-from decouple import config
+
 from pathlib import Path
 import os
 
@@ -44,7 +42,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'afeng.urls'
@@ -124,8 +120,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [    BASE_DIR / "static",]
 
-STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 EMAIL_HOST = 'smtp.gmail.com'
@@ -135,5 +129,3 @@ EMAIL_HOST_PASSWORD = '30071993'
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
